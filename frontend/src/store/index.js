fde85dcf84986e618d96c26c8a5235a4a333af36
Alexander Raunert <alexander.raunert@gmail.com>
2021-02-16 14:04:51 +0100

    vuex-persistedstate keeps users logged in between refreshes

--- frontend/src/store/index.js
+++ frontend/src/store/index.js
@@ -1,10 +1,12 @@
 import { createStore } from "vuex";
+import createPersistedState from "vuex-persistedstate";
 import auth from "./modules/auth";
 import playlists from "./modules/playlist";
 
 export default createStore({
   modules: {
     auth,
-    playlists
+    playlists,
   },
+  plugins: [createPersistedState()],
 });
